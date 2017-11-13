4e6bbe2c2bb4da1dbf2f02843bf5e366a39872c5
hwt2016 <66295112@qq.com>
2017-11-13 21:00:59 +0800

    增加公告模块

---
+++ src/main/java/com/controller/AnnouncementController.java
@@ -0,0 +1,71 @@
+package com.controller;
+
+import com.em.StatusEnum;
+import com.entity.AnnouncementDO;
+import com.service.AnnouncementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by sa on 2017-11-13.
+ */
+@Controller
+@RequestMapping(value = "/announcement")
+public class AnnouncementController {
+
+    @Autowired
+    AnnouncementService announcementService;
+
+    //公告查看                announcementRead
+    @RequestMapping(value = "/announcementRead",method = RequestMethod.GET)
+    public String announcementRead(ModelMap modelMap){
+        AnnouncementDO announcementDO =announcementService.announcementRead();
+        modelMap.addAttribute("announcementnew",announcementDO);
+        return "admin/announcement/announcementRead";
+    }
+
+    //协议查看
+    @RequestMapping(value = "/protocolRead",method = RequestMethod.GET)
+    public String protocolRead(){
+
+        return "admin/announcement/protocolRead";
+    }
+
+    //公司简介查看
+    @RequestMapping(value = "/aboutUsRead",method = RequestMethod.GET)
+    public String aboutUsRead(){
+
+        return "admin/announcement/aboutUsRead";
+    }
+
+
+    /**
+     * 插入一条新的公告信息
+     */
+    @RequestMapping(value = "/announcementInsert",method = RequestMethod.GET)
+    public String announcementInsert(){
+        return "admin/announcement/announcementInsert";
+    }
+
+    /**
+     * 插入一条新的公告信息
+     * @param announcementDO（title和content）
+     * @return
+     */
+    @RequestMapping(value = "/announcementInsert",method = RequestMethod.POST)
+    @ResponseBody
+    public String announcementInsert(AnnouncementDO announcementDO){
+        announcementDO.setStatus(StatusEnum.NORMAL.code());
+        if(announcementService.announcementInsert(announcementDO))
+            return "true";
+        else
+            return "false";
+    }
+
+
+
+}
---
+++ src/main/java/com/service/AnnouncementService.java
@@ -0,0 +1,65 @@
+package com.service;
+
+import com.entity.AnnouncementDO;
+import com.entity.AnnouncementDOExample;
+import com.mapper.AnnouncementDOMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by sa on 2017-11-13.
+ */
+@Service
+public class AnnouncementService {
+
+    @Autowired
+    AnnouncementDOMapper announcementDOMapper;
+
+    /**
+     * 提取最新公告信息
+     * @return
+     */
+    public AnnouncementDO announcementRead(){
+        AnnouncementDOExample announcementDOExample=new AnnouncementDOExample();
+        AnnouncementDOExample.Criteria criteria = announcementDOExample.createCriteria();
+        announcementDOExample.setOrderByClause("id DESC");
+        List<AnnouncementDO> announcementDOS = announcementDOMapper.selectByExample(announcementDOExample);
+        if(announcementDOS!=null)
+            return announcementDOS.get(0);
+
+        return null;
+    }
+
+    /**
+     *根据announcementd修改公告信息
+     * @param announcementDO
+     * @return
+     */
+    public boolean announcementUpdate(AnnouncementDO announcementDO){
+        announcementDO.setUpdatetime(new Date(System.currentTimeMillis()));
+        announcementDOMapper.updateByPrimaryKeySelective(announcementDO);
+        return true;
+
+    }
+
+    /**
+     * 插入一条公告信息
+     * @param announcementDO
+     * @return
+     */
+    public boolean announcementInsert(AnnouncementDO announcementDO){
+        announcementDO.setCreatetime(new Date(System.currentTimeMillis()));
+        announcementDO.setUpdatetime(new Date(System.currentTimeMillis()));
+        try{
+            announcementDOMapper.insert(announcementDO);
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+}
